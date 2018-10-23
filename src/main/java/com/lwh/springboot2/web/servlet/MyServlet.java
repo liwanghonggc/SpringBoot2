fbe6764357778eafede73d786b0e7a2625711ee2
lwh <2391519868@qq.com>
2018-10-23 22:28:18 +0800

    SpringBoot2学习

--- src/main/java/com/lwh/springboot2/web/servlet/MyServlet.java
+++ src/main/java/com/lwh/springboot2/web/servlet/MyServlet.java
@@ -1,5 +1,6 @@
 package com.lwh.springboot2.web.servlet;
 
+import javax.servlet.AsyncContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,6 +19,17 @@ public class MyServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("Hello World");
+        //非异步方式
+        //resp.getWriter().println("Hello World");
+
+        //异步方式
+        AsyncContext asyncContext = req.startAsync();
+        asyncContext.start(() -> {
+            try {
+                resp.getWriter().println("Hello World");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
