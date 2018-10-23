9753a205901a95f2dcea15840280f8969e4b7715
lwh <2391519868@qq.com>
2018-10-23 22:21:55 +0800

    SpringBoot2学习

--- src/main/java/com/lwh/springboot2/Springboot2Application.java
+++ src/main/java/com/lwh/springboot2/Springboot2Application.java
@@ -2,8 +2,13 @@ package com.lwh.springboot2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+/**
+ * @ServletComponentScan 注册Servlet
+ */
 @SpringBootApplication
+@ServletComponentScan(basePackages = "com.lwh.springboot2.web.servlet")
 public class Springboot2Application {
 
     public static void main(String[] args) {
--- src/main/java/com/lwh/springboot2/web/servlet/MyServlet.java
+++ src/main/java/com/lwh/springboot2/web/servlet/MyServlet.java
@@ -1,9 +1,23 @@
 package com.lwh.springboot2.web.servlet;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * @author lwh
  * @date 2018-10-23
  * @desp
  */
-public class MyServlet {
+
+@WebServlet(urlPatterns = "/my/servlet")
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("Hello World");
+    }
 }
